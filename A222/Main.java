97cd8fa7670f61aae2810f515d76c60886e42012
11249a222-lgtm <11249a222@kanchiuniv.ac.in>
2026-08-20 13:17:06 +0530

    Add files via upload

---
+++ A222/Main.java
@@ -0,0 +1,11 @@
+import studentInfo.tudent;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        tudent s = new tudent();
+
+        s.displayInfo();
+    }
+}
---
+++ A222/studentInfo/tudent.java
@@ -0,0 +1,10 @@
+package studentInfo;
+
+public class tudent {
+
+    public void displayInfo() {
+        System.out.println("Student Name: Himabindu");
+        System.out.println("Roll Number: 101");
+        System.out.println("Course: Computer Science");
+    }
+}
